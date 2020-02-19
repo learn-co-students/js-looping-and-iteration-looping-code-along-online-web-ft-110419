8080064f91a09c41323eef598e3f787fdae3e01e
Nethelbert <gilblasse@gmail.com>
2020-02-19 02:31:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  let arry = [];
+  for (let i = 0; i <= writeCards.length; i++) {
+  arry.push(`Thank you, ${names[i]} for the wonderful ${event} gift!`);
+  }
+ 
+  return arry;
+}
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
