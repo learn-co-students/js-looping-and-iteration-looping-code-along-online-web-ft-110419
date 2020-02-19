8b147b083869aba03293ad3f07e226ca8a5271ac
Nethelbert <gilblasse@gmail.com>
2020-02-19 02:58:58 +0000

    Done.

--- index.js
+++ index.js
@@ -3,9 +3,17 @@
 function writeCards(names, event) {
   let arry = [];
   for (let i = 0; i <= writeCards.length; i++) {
-  arry.push(`Thank you, ${names[i]} for the wonderful ${event} gift!`);
+  arry.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
  
   return arry;
 }
 writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(num) {
+  for (let i = num; i <= 0; i--) {
+    console.log(i)
+  }
+}
+
+countDown(10)
