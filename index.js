a5eb4e1eab1b893ce85d901ae92acab9f68d0052
LucyWeltner <lucyiweltner@gmail.com>
2020-02-18 22:19:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function writeCards(array, word) {
+	let messagesArray = [];
+	for (let i=0; i < array.length; i++) {
+		messagesArray.push(`Thank you, ${array[i]}, for the wonderful ` + word + " gift!");
+	}
+	return messagesArray;
+
+}
+
+function countDown(int) {
+	while (int >= 0) {
+		console.log(int);
+		int -= 1;
+	}
+}
 // Code your solutions in this file
