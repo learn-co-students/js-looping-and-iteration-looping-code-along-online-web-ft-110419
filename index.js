b2ccb59842486ad5f864850bcb9b028a9b309ed9
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-02-18 23:03:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names){
+    let thanksYous = [];
+    for (let i = 0; i < names.length; i++){
+       thanksYous.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    };
+    return thanksYous;
+}
+
+function countDown(i){
+    while (i >= 0){
+        console.log(i--);
+    }
+}
