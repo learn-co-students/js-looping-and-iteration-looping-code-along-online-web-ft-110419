75dee468492a7d0f9de50304a05dca4770bd4374
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-02-20 22:47:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+//[ 'Thank you, Lisa, for the wonderful surprise gift!', 'Thank you, Kaitlin, for the wonderful surprise gift!', 'Thank you, Jan, for the wonderful surprise gift!' ]
+//names = [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' )
+
+
+
+function writeCards(names) {
+  let array = []
+  for (let i = 0; i < names.length; i++) {
+    array.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+  }
+  return array;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number)
+    number--
+  }
+}
