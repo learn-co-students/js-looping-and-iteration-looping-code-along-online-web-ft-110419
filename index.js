bbd2ed74ef2ebcb6b33fc0b49b72699a010780f2
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-20 14:02:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arr, eventName) {
+  const msgs = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    msgs.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return msgs;
+}
+
+function countDown(n) {
+  while (n >= 0) {
+    console.log(n);
+    n--;
+  }
+}
