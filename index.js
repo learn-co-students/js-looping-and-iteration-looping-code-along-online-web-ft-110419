53d1360abdb849b0dad1a088ccffc203d2f43e4a
C-Mike576 <m.calkins576@gmail.com>
2020-02-21 15:15:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(nameArray, eventName) {
+    let thankArray = []
+    for (let i = 0; i < nameArray.length; i++) {
+        thankArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return thankArray
+}
+
+function countDown(chosenNum) {
+    let countNum = chosenNum
+    while (countNum > -1) {
+        console.log(countNum)
+        countNum--        
+    }
+}
