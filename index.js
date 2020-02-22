38c7162bdf68cbbcf938012b58772d7f25088924
Ethan <ethan_gustafson@outlook.com>
2020-02-22 15:45:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+// const array = ['Lisa', 'Kaitlin', 'Jan'];
+// let event = "surprise";
+
+function writeCards(array, event) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown() {
+    let i = 0;
+    while (i < 11) {
+        console.log(i++);
+    }
+}
